3a45e8e805fc9e33731d317a64b18c72e22dbb16
Krek0 <kreko@posteo.net>
2024-05-06 22:17:17 +0200

    add print, add conjonction of expression with ";" and cleanup proprity of grammar for operators of priority 0

--- src/eval.ml
+++ src/eval.ml
@@ -3,21 +3,29 @@ open Type
 (*x by v in e*)
 let rec sub e x v = match e with
   | Var y when y = x -> v
-  | Number _ | Op _ | Var _ | True | False -> e
+  | Number _ | Op _ | Var _ | True | False | Unit -> e
   | Fun (y,_) when y = x -> e
   | Fun (y,e1) -> Fun(y,sub e1 x v)
   | App (e1,e2) -> App(sub e1 x v, sub e2 x v)
   | Pair (e1,e2) -> Pair(sub e1 x v, sub e2 x v)
   | Let(y,e1,e2) when y = x -> Let(y,sub e1 x v,e2)
   | Let(y,e1,e2) -> Let(y,sub e1 x v, sub e2 x v)
+  | Multiple(e1,e2) -> Multiple(sub e1 x v,sub e2 x v)
 
 let rec eval e = match e with
-  | Number _ | Op _ | Fun _ | True | False -> e
+  | Number _ | Op _ | Fun _ | True | False | Unit -> e
+  | Multiple(e1,e2) -> ignore @@ eval e1; eval e2
   | Pair(e1,e2) -> Pair(eval e1, eval e2)
   | Let(x,e1,e2) -> eval @@ sub e2 x (eval e1)
   | App (e1,e2) -> (
       match eval e1 with
         | Fun(x,e) -> eval @@ sub e x (eval e2)
+        | Op "print" -> (
+          match eval e2 with
+            | Number i -> print_int i; print_newline (); Unit
+            | True -> print_string "true"; print_newline (); Unit
+            | False -> print_string "false"; print_newline (); Unit
+            | _ -> failwith "eval print" )
         (*Lazy evaluation for && and ||*)
         | Op "&&" -> (
           match e2 with
